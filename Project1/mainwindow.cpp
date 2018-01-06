40e5dc599dc456546ba766b9fce58c136fa63b93
RahulChauhan <rahulchauhan.mps@yahoo.com>
2018-01-06 23:09:57 +0530

    basic Application to undersatnd the QGraphicsView,QGraphicsViewScene,QGraphicsItem

---
+++ Project1/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include <QApplication>
+
+/*
+
+
+  QGraphicsScene:
+  QGraphicsItem:
+  QGraphicsView:
+
+ *
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
